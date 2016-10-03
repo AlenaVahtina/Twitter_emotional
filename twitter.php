79e491900fd9e0d3fd6cf02ea0ef997e93b891d6
Alena <alenavahtina@gmail.com>
2016-10-04 00:54:12 +0300

    proof of concept complete

--- twitter.php
+++ twitter.php
@@ -1,7 +1,43 @@
 <?php
-    //  âñÿ ïðîöåäóðà ðàáîòàåò íà ñåññèÿõ. Èìåííî â íåé õðàíÿòñÿ äàííûå  ïîëüçîâàòåëÿ, ïîêà îí íàõîäèòñÿ íà ñàéòå. Î÷åíü âàæíî çàïóñòèòü èõ â  ñàìîì íà÷àëå ñòðàíè÷êè!!!
-    session_start();
-    ?>
+session_start();
+$db = new SQLite3('twitter.db');
+
+if($_SESSION['current_message_id']=="0"){
+    $results = $db->query('SELECT id_message FROM apprisal where id_user='.$_SESSION['id'].' order by id_message');
+    $myrow = $results->fetchArray();
+    $_SESSION['current_message_id']=$myrow['id_message'];
+}
+
+if (isset($_POST['emotional'])) {
+  $emotional_count=0;
+  $inf_count=0;
+
+  if($_POST['emotional']=='positive'){
+    $emotional_count=1;
+  } 
+    if($_POST['emotional']=='neutral'){
+    $emotional_count=2;
+  } 
+    if($_POST['emotional']=='negative'){
+    $emotional_count=3;
+  } 
+    if($_POST['emotional']=='uninformative'){
+    $inf_count=1;
+  } 
+
+  $db->query('INSERT INTO apprisal(id_user, id_message, emotional_color, inf_color) VALUES('.$_SESSION['id'].', '.$_SESSION['current_message_id'].', '.$emotional_count.', '.$inf_count.' )');
+  $_SESSION['current_message_id']=$_SESSION['current_message_id']+1;
+} 
+
+$twitt_text= $db->query('SELECT * FROM message where id='.$_SESSION['current_message_id']);//извлекаем из базы message
+$twittet_row = $twitt_text->fetchArray();
+$_SESSION['m_text']=$twittet_row['m_text']; 
+
+// $bd_apprisal= $db->query('SELECT * FROM apprisal');//извлекаем из базы apprisal
+// $apprisal_row=$bd_apprisal->fetchArray();
+// $_SESSION['emotional_color']=$twittet_row['emotional_color'];
+// $_SESSION['inf_color']=$twittet_row['inf_color'];  
+?>
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -10,32 +46,39 @@
 </head>
 <body>
 
-<IMG scr="twitter_icon.png">
+  <style>
+   p {
+    border: 1px solid blue;
+    padding: 10px;
+   }
+  </style>
 
- <LABLE><TEXTAREA type="text" id="twitter"></TEXTAREA></LABLE>
-</div>
+<p><img src="twitter_icon.png"></p>
+
+
+ <p><label type="text" name="m_text">
+    <?php
+    echo $_SESSION['m_text'];
+    ?>
+ </label></p>
 
 <form action="/twitter.php" method="post">
-<?php 
-  $db = new SQLite3('twitter.db');
-?>
-<input type="text" name="login" size=14 />
   <fieldset>
     <legend>Select a Location: </legend>
 
-    <input type="radio" name="radio-1" id="radio-1">
-    <label for="radio-1">Positive</label>
+    <input type="radio" name="emotional" value="positive">
+    <label for="positive">Positive</label>
 
-    <input type="radio" name="radio-1" id="radio-2">
-    <label for="radio-2">Neutral</label>
+    <input type="radio" name="emotional" value="neutral">
+    <label for="neutral">Neutral</label>
 
-    <input type="radio" name="radio-1" id="radio-3">
-    <label for="radio-3">Negative</label>
+    <input type="radio" name="emotional" value="negative">
+    <label for="negative">Negative</label>
   </fieldset>
 
   <br>
-  <input type="radio" name="radio-1" id="radio-4">
-  <label for="radio-4">Uninformative</label>
+  <input type="radio" name="emotional"  value="uninformative">
+  <label for="uninformative">Uninformative</label>
 
   <br>
   <td><input type="submit" value="Ok" /></td>
