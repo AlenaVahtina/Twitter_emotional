3d0d357e1a05049ea9dee23b547b0cb5784d3c47
Alena <alenavahtina@gmail.com>
2016-10-07 00:06:12 +0300

    add upload form

---
+++ upload_file.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form enctype="multipart/form-data" action="server_upload" method="POST">
+    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <!-- Название элемента input определяет имя в массиве $_FILES -->
+    Отправить этот файл: <input name="userfile" type="file" />
+    <input type="submit" value="Send File" />
+</form>
+</body>
+</html>
